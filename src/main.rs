f9474bf4783420e08f7d575b3683264bd05f765c
Boris-Chengbiao Zhou <bobo1239@web.de>
2025-02-08 14:34:44 +0100

    Adjust the short argument for setting the port
    Keep the old argument to prevent user breakage.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ struct Args {
     #[arg(short('a'), long, default_value_t = IpAddr::V4(Ipv4Addr::UNSPECIFIED))]
     websocket_server_bind_address: IpAddr,
 
-    #[arg(short, long, default_value_t = 6677)]
+    #[arg(short('p'), visible_short_alias('w'), long, default_value_t = 6677)]
     websocket_server_port: u16,
 }
 
